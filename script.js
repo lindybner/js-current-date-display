af693b8d18ccbf598e1ca1f06f51a6b67f888746
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-15 10:51:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 // var
 const output = document.querySelector("#output");
+const newDate = new Date();
+let mm;
+let dd;
+let yyyy;
+let outputDate;
+
+// fn
+function setMonth() {
+  mm = newDate.getMonth;
+  return mm;
+}
+
+function setDay() {
+  dd = newDate.getDate;
+  return dd;
+}
+
+function setYear() {
+  yyyy = newDate.getFullYear;
+  return yyyy;
+}
+
+// set outputDate
+function setOutPutDate(mm, dd, yyyy) {
+  outputDate = `${mm}-${dd}-${yyyy}`;
+  return outputDate;
+}
+
+// output date to output
+function outputDate(outputDate) {
+  output.innerHTML = outputDate;
+}
+
+// call outputDate fn
+outputDate();
